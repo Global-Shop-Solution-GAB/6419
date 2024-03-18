40d2646770caa8d88ca13a53004f08a3ccadbc24
Service Web <sw@gssmail.com>
2024-03-18 14:54:14 -0500

    Commit changes in C:\Websites\Git\emarlow\6419\GCG_6419_Punch_Mstr.sql

---
+++ GCG_6419_Punch_Mstr.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "GCG_6419_Punch_Mstr"(
+ "Shape" CHAR(70),
+ "Punch" CHAR(75),
+ "A" CHAR(15),
+ "B" CHAR(15),
+ "C" CHAR(15),
+ "C2" CHAR(15),
+ "D" CHAR(15),
+ "E" CHAR(15),
+ "F" CHAR(15),
+ "G" CHAR(15),
+ "I" CHAR(15),
+ "STATE" CHAR(15));
